1ec2bc9913bf34bf9780874a325519764d1d7d10
pimperator <github@lukacs.lu>
2016-08-17 18:13:45 +0200

    some packages are debian-flavoured only

--- manifests/prepare.pp
+++ manifests/prepare.pp
@@ -2,15 +2,18 @@ class suricata::prepare {
 	package { 'ethtool':
 		ensure => installed,
 	}
-	package {'libpcre3':
-		ensure => installed,
+
+	case $::osfamily {
+		'Debian': {
+			package {['libpcre3','libpcre3-dbg','libpcre3-dev']:
+				ensure => installed,
+				}
 	}
-	package {'libpcre3-dbg':
-		ensure => installed,
+		'RedHat': {
+			package{'pcre':
+				ensure => installed,
+				}
 	}
-	package {'libpcre3-dev':
-		ensure => installed,
-	}	
 
 	exec { 'set_promiscuous_mode':
 		command => "/sbin/ifconfig ${suricata::interface} promisc",
